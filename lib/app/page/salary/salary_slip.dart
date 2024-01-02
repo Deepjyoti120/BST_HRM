c775c696d1628663ee4a9f07eeb7b7c1ae065724
Deepjyoti120 <deepjyoti120281@gmail.com>
2024-01-02 21:52:05 +0530

    done

--- lib/app/page/salary/salary_slip.dart
+++ lib/app/page/salary/salary_slip.dart
@@ -1,5 +1,6 @@
 import 'package:bsthrm/app/widget/pdf/salary_slip_create.dart';
 import 'package:flutter/material.dart';
+import 'package:pdf/pdf.dart';
 import 'package:printing/printing.dart';
 
 class SalarySlip extends StatefulWidget {
@@ -14,8 +15,21 @@ class _SalarySlipState extends State<SalarySlip> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: PdfPreview(
-        build: (format) => generateSalarySlip(format),
+      // body: PdfPreview(
+      //   build: (format) => generateSalarySlip(PdfPageFormat.a4),
+      // ),
+      // add button to download unit8List pdf
+      body: Column(
+        children: [
+          ElevatedButton(
+              onPressed: () async {
+                await Printing.sharePdf(
+                  bytes: await generateSalarySlip(PdfPageFormat.a4),
+                  filename: 'my-document.pdf',
+                );
+              },
+              child: Text("data"))
+        ],
       ),
     );
   }
